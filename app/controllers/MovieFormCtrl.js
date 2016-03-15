89ad29a4e085a081cecf136002f2cd3796b220eb
Mark Zachary <MarkZaWill@gmail.com>
2016-03-15 17:51:50 -0500

    Adding some inital logic for reference to what it would need to look like

---
+++ app/controllers/MovieFormCtrl.js
@@ -0,0 +1,44 @@
+"use strict";
+
+MusicHistory.controller("SongFormCtrl",
+[
+  "$scope",
+  "$location",
+  "$http",
+
+  function ($scope, $location, $http)  {
+
+    // Default property values for keys bound to input fields
+    $scope.newSong = {
+      name: "",
+      albumName: "",
+      albumYear: "",
+      artist: ""
+    };
+
+    // Function bound to the Add Song button in the view template
+    $scope.addSong = function () {
+
+      // POST the song to Firebase
+      $http.post(
+        "https://nss-demo-instructor.firebaseio.com/songs.json",
+
+        // Remember to stringify objects/arrays before
+        // sending them to an API
+        JSON.stringify({
+          name: $scope.newSong.name,
+          album: {
+            name: $scope.newSong.albumName,
+            year: $scope.newSong.albumYear,
+          },
+          artist: $scope.newSong.artist
+        })
+
+      // The $http.post() method returns a promise, so you can use then()
+      ).then(
+        () => $location.url("/songs/"),      // Handle resolve
+        (response) => console.log(response)  // Handle reject
+      );
+    };
+  }
+]);
